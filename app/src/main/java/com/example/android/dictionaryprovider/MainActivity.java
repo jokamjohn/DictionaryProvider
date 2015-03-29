491bb7f6dfc46dbf01ffb0898aa28c145aa364ac
jokamjohn <johnkagga@gmail.com>
2015-03-29 21:18:08 +0300

    List of words

--- app/src/main/java/com/example/android/dictionaryprovider/MainActivity.java
+++ app/src/main/java/com/example/android/dictionaryprovider/MainActivity.java
@@ -2,11 +2,9 @@ package com.example.android.dictionaryprovider;
 
 import android.content.ContentResolver;
 import android.database.Cursor;
+import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.TextView;
 
 
@@ -27,14 +25,21 @@ public class MainActivity extends ActionBarActivity {
 
         try {
 
-            dicttext.setText("Your user dictionary contains " );
+            dicttext.setText("Your user dictionary contains " + cursor.getCount() + " words\n" );
+            // Get the index of the columns using UserDictionary.Words constants,
+            // which contain the headers of the columns.
             int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
+            int idcolumn = cursor.getColumnIndex(UserDictionary.Words._ID);
+            int freqcolumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
+
 
             while (cursor.moveToFirst()){
                 // Use that index to extract the String value of the word
                 // at the current row the cursor is on.
+                int id = cursor.getInt(idcolumn);
+                int freq =cursor.getInt(freqcolumn);
                 String word =cursor.getString(wordColumn);
-                dicttext.append(("\n" + word));
+                dicttext.append(("\n" + id + "-" +freq+ "-" + word));
             }
         }
         finally {
@@ -48,25 +53,25 @@ public class MainActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.menu_main, menu);
+//        return true;
+//    }
+//
+//    @Override
+//    public boolean onOptionsItemSelected(MenuItem item) {
+//        // Handle action bar item clicks here. The action bar will
+//        // automatically handle clicks on the Home/Up button, so long
+//        // as you specify a parent activity in AndroidManifest.xml.
+//        int id = item.getItemId();
+//
+//        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
+//
+//        return super.onOptionsItemSelected(item);
+//    }
 }
