4b6efb913f1e3f292b5c75bffe880868b88796a3
jokamjohn <johnkagga@gmail.com>
2015-03-29 22:24:38 +0300

    Add SimpleCursorAdapter

--- app/src/main/java/com/example/android/dictionaryprovider/MainActivity.java
+++ app/src/main/java/com/example/android/dictionaryprovider/MainActivity.java
@@ -5,47 +5,36 @@ import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.UserDictionary;
 import android.support.v7.app.ActionBarActivity;
-import android.widget.TextView;
+import android.widget.ListView;
+import android.support.v4.widget.SimpleCursorAdapter;
+
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final String [] columns = new String[] {UserDictionary.Words.WORD,UserDictionary.Words.FREQUENCY};
+
+    private static final int [] toview = new int[] {android.R.id.text1,android.R.id.text2};
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //Text view to populate the words
-        TextView dicttext = (TextView)findViewById(R.id.dictionary_text_view);
+        //List view to populate the words
+        ListView dictListView = (ListView)findViewById(R.id.dictionary_list_view);
 
         //Get the ContentResolver which will send a message to the ContentProvider
         ContentResolver resolver =getContentResolver();
         //Get a Cursor containing all of the rows in the Words table
         Cursor cursor =resolver.query(UserDictionary.Words.CONTENT_URI,null,null,null,null);
 
-        try {
+        // Initiating the simpleCursorAdapter
+        SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,android.R.layout.two_line_list_item,
+                cursor,columns,toview,0);
 
-            dicttext.setText("Your user dictionary contains " + cursor.getCount() + " words\n" );
-            // Get the index of the columns using UserDictionary.Words constants,
-            // which contain the headers of the columns.
-            int wordColumn = cursor.getColumnIndex(UserDictionary.Words.WORD);
-            int idcolumn = cursor.getColumnIndex(UserDictionary.Words._ID);
-            int freqcolumn = cursor.getColumnIndex(UserDictionary.Words.FREQUENCY);
+        dictListView.setAdapter(adapter);
 
-
-            while (cursor.moveToFirst()){
-                // Use that index to extract the String value of the word
-                // at the current row the cursor is on.
-                int id = cursor.getInt(idcolumn);
-                int freq =cursor.getInt(freqcolumn);
-                String word =cursor.getString(wordColumn);
-                dicttext.append(("\n" + id + "-" +freq+ "-" + word));
-            }
-        }
-        finally {
-            // Always close your cursor to avoid memory leaks
-            cursor.close();
-        }
 
 
 
